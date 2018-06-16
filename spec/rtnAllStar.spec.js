31497de9d06f7e192aa3fe8e492d54be22c06bca
DanSoup <dansoup@hotmail.co.uk>
2018-06-17 00:42:59 +0100

    start of the journey. 2232 characters

--- spec/rtnAllStar.spec.js
+++ spec/rtnAllStar.spec.js
@@ -1,11 +1,11 @@
 const {expect} = require('chai')
 const allStarText = require('../allStarTxt')
-const rtnAllStar = require('../rtnAllStar')
+const allStar = require('../allStar')
 const fs = require('fs')
 
 describe('', () => {
     it('Matches exactly the lyrics of All Star', () => {
-        expect(rtnAllStar()).to.equal(allStarText)
-        console.log(`rtnAllStar.js is ${fs.readFileSync('./rtnAllStar.js', 'UTF8').length} characters long.`)
+        expect(allStar()).to.equal(allStarText)
+        console.log(`rtnAllStar.js is ${fs.readFileSync('./allStar.js', 'UTF8').length} characters long.`)
     })
 })
